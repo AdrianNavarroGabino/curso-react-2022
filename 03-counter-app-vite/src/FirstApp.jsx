e58824dcba4fdd2cb3cb9ffc6e58524e3c2556fe
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-10-19 11:54:46 +0200

    Impresión de variables en el HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,16 @@
+const newMessage = {
+    message: 'Hola, mundo',
+    title: 'Adrián'
+};
+
+const getResult = (a, b) => a + b;
+
 export const FirstApp = () => {
     return (
         <>
-            <h1>Tarea - Crear un nuevo componente</h1>
+            <h1>Hola, mundo</h1>
+            <code>{JSON.stringify(newMessage)}</code>
+            <p>{getResult(3, 6)}</p>
             <p>Soy un subtítulo</p>
         </>
     );
