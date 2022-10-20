0d950cbd12e5a6e9c473c75609992fd2863a8bf1
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-10-20 13:44:18 +0200

    Eventos

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,9 +1,17 @@
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({value}) => (<>
-<h1>CounterApp</h1>
-<h2>{value}</h2>
-</>);
+export const CounterApp = ({value}) => {
+    
+    const handleAdd = () => console.log("+1");
+
+    return (<>
+        <h1>CounterApp</h1>
+        <h2> { value } </h2>
+        <button onClick={ handleAdd }>
+            +1
+        </button>
+    </>);
+}
 
 CounterApp.propTypes = {
     value: PropTypes.number.isRequired
