b332e0a6966852fb07579655550519de8ef9f967
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-10-20 13:27:58 +0200

    Tarea - Componente CounterApp

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
 import {FirstApp} from './FirstApp';
 import './styles.css';
+import { CounterApp } from './CounterApp';
+
+/*ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <FirstApp title="Hola, mundo" subtitle={123}/>
+    </React.StrictMode>
+)*/
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <FirstApp title="Hola, mundo" subtitle={123}/>
+        <CounterApp value={0}/>
     </React.StrictMode>
 )
