158525f5dbdce057a7a5b29ad53b376581615303
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-10-25 19:47:34 +0200

    handleSubstract y handleReset

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -7,13 +7,15 @@ export const CounterApp = ({value}) => {
     
     //const handleAdd = () => setCounter(counter + 1);
     const handleAdd = () => setCounter(c => c + 1 );
+    const handleSubstract = () => setCounter(c => c - 1);
+    const handleReset = () => setCounter(value);
 
     return (<>
         <h1>CounterApp</h1>
         <h2> { counter } </h2>
-        <button onClick={ handleAdd }>
-            +1
-        </button>
+        <button onClick={ handleAdd }>+1</button>
+        <button onClick={ handleSubstract }>-1</button>
+        <button onClick={ handleReset }>Reset</button>
     </>);
 }
 
