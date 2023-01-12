047ccc662715001ef59a38919ca7e2eb97cf6328
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-12 19:41:57 +0100

    GifExpertApp - Component

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,13 @@
 export const GifExpertApp = () => {
     return (
         <>
+            {/* Título */}
             <h1>Gif Expert App</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+            {/* Gif item */}
         </>
     )
 }
