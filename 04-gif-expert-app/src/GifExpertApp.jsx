4e346ab6638e205d2bf0fc14ac27aa54ddae2858
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-14 19:27:34 +0100

    Comunicación entre componentes

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -14,10 +14,9 @@ export const GifExpertApp = () => {
             <h1>Gif Expert App</h1>
 
             {/* Input */}
-            <AddCategory />
+            <AddCategory setCategories={setCategories} />
 
             {/* Listado de Gif */}
-            <button onClick={onAddCategory}>Agregar</button>
             <ol>
                 {categories.map(category => <li key={category}>{category}</li>)}
             </ol>
--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
-export const AddCategory = () => {
+export const AddCategory = ({ setCategories }) => {
     const [inputValue, setInputValue] = useState('One Punch')
 
     const onInputChange = event => setInputValue(event.target.value);
 
     const onSubmit = event => {
         event.preventDefault();
-        console.log(inputValue);
+        if (inputValue.trim().length <= 1) {
+            return;
+        }
+
+        setCategories(categories => [inputValue, ...categories]);
+        setInputValue('');
     }
 
     return (
