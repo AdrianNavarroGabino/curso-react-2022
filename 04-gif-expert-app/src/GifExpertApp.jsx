e5f0c869b48c0c3c145b6d7d05c10f8e0969aafe
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-15 21:09:56 +0100

    Validar que sean únicos los nombres

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -5,6 +5,9 @@ export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch']);
 
     const onAddCategory = newCategory => {
+        if (categories.includes(newCategory)) {
+            return;
+        }
         setCategories([newCategory, ...categories]);
     }
 
