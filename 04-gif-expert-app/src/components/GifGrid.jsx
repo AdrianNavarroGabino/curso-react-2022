9b8e91e49527461d3bf037b182528304dafa4931
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-17 18:32:18 +0100

    useEffect

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs"
 
 
 export const GifGrid = ({ category }) => {
-    getGifs(category);
+    const [gifs, setGifs] = useState([])
+
+    useEffect(() => {
+        getGifs(category).then(console.log)
+    }, []);
 
     return (
         <div key={category}>
