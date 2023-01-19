94c9c7d482f577650c094594d53ca276985290b0
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-19 21:20:31 +0100

    Mostrar mensaje de carga

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -9,6 +9,7 @@ export const GifGrid = ({ category }) => {
     return (
         <div key={category}>
             <h3>{category}</h3>
+            {isLoading && <h2>Cargando...</h2>}
             <div className='card-grid'>
                 {images.map(image => <GifItem key={image.id} {...image} />)}
             </div>
