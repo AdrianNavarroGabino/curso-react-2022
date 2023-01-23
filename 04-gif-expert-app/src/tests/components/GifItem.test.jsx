f732d0daad59dc2b44fd49cd4e631024fba57df6
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-23 21:01:41 +0100

    Pruebas del componente - GifGridItem

--- 04-gif-expert-app/src/tests/components/GifItem.test.jsx
+++ 04-gif-expert-app/src/tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../components/GifItem";
 
 describe('Pruebas en GifItem', () => {
@@ -8,5 +8,18 @@ describe('Pruebas en GifItem', () => {
     test('Deber hacer match con el snapshot', () => {
         const { container } = render(<GifItem title={title} url={url} />);
         expect(container).toMatchSnapshot();
+    });
+
+    test('Debe mostrar la imagen con el URL y el ALT indicado', () => {
+        render(<GifItem title={title} url={url} />);
+        const { src, alt } = screen.getByRole('img');
+
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('Debe mostrar el título en el componente', () => {
+        render(<GifItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
     });
 });
