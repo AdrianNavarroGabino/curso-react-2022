a132fc79910bca723f65adae69b06e1643b38942
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-25 21:03:59 +0100

    Jest functions

--- 04-gif-expert-app/src/tests/components/AddCategory.test.jsx
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.jsx
@@ -12,8 +12,9 @@ describe('Pruebas en AddCategory', () => {
 
     test('Debe llamar onNewCategory si el input tiene un valor', () => {
         const inputValue = 'Saitama';
+        const onNewCategory = jest.fn();
 
-        render(<AddCategory onNewCategory={() => { }} />);
+        render(<AddCategory onNewCategory={onNewCategory} />);
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
 
@@ -21,5 +22,19 @@ describe('Pruebas en AddCategory', () => {
         fireEvent.submit(form);
 
         expect(input.value).toBe('');
+        expect(onNewCategory).toHaveBeenCalled();
+        expect(onNewCategory).toHaveBeenCalledTimes(1);
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+    });
+
+    test('No debe llamar onNewCategory si el input está vacío', () => {
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={onNewCategory} />);
+
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+
+        expect(onNewCategory).not.toHaveBeenCalled();
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
     });
 });
