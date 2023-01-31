72fe67384708e88f174dd53b7d4d7c2a3cbb7326
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-31 19:09:42 +0100

    useEffect - Precauciones

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -1,17 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+    const onMouseMove = ({ x, y }) => {
+        setCoords({ x, y });
+    }
+
     useEffect(() => {
-        console.log('Message Mounted');
+        window.addEventListener('mousemove', onMouseMove);
 
         return () => {
-            console.log('Message unmounted');
+            window.removeEventListener('mousemove', onMouseMove);
         }
     }, []);
 
     return (
         <>
             <h3>Usuario ya existe</h3>
+            {JSON.stringify(coords)}
         </>
     )
 }
