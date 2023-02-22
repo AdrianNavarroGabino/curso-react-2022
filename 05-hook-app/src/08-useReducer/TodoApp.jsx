053d0ba4c08d6d86b924285950ed0121468cd5b4
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-02-22 21:16:23 +0100

    Borrar un TODO

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -36,6 +36,13 @@ export const TodoApp = () => {
         dispatch(action);
     }
 
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id,
+        });
+    }
+
     return (
         <>
             <h1>TodoApp 10, <small>pendientes: 2</small></h1>
@@ -43,7 +50,7 @@ export const TodoApp = () => {
 
             <div className="row">
                 <div className="col-7">
-                    <TodoList todos={ todos } />
+                    <TodoList todos={ todos } onDeleteTodo={ handleDeleteTodo } />
                 </div>
 
 
--- 05-hook-app/src/08-useReducer/TodoList.jsx
+++ 05-hook-app/src/08-useReducer/TodoList.jsx
@@ -1,12 +1,11 @@
-export const TodoList = ({ todos }) => {
+import { TodoItem } from "./TodoItem"
+
+export const TodoList = ({ todos, onDeleteTodo }) => {
     return (
         <ul className="list-group">
             {
                 todos.map((todo) => (
-                    <li key={ todo.id } className="list-group-item d-flex justify-content-between">
-                        <span className="align-self-center">{ todo.description }</span>
-                        <button className="btn btn-danger">Borrar</button>
-                    </li>
+                    <TodoItem key={ todo.id } todo={ todo } onDeleteTodo={ onDeleteTodo } />
                 ))
             }
         </ul>
