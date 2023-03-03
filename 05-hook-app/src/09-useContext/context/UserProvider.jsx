82d46c36f85c20d9cf7cb1e602213f2fd02f47d2
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-03 07:54:24 +0100

    useContext

--- 05-hook-app/src/09-useContext/context/UserProvider.jsx
+++ 05-hook-app/src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import { UserContext } from './UserContext';
 
-const user = {
-    id: 123,
-    nombre: 'Adrian',
-    password: 'abcde',
-}
+// const user = {
+//     id: 123,
+//     nombre: 'Adrian',
+//     password: 'abcde',
+// }
 
 export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState('');
+
     return (
-        <UserContext.Provider value={ { hola: 'Mundo', user } }>
+        // <UserContext.Provider value={ { hola: 'Mundo', user } }>
+        <UserContext.Provider value={ { user, setUser } }>
             { children }
         </UserContext.Provider>
     );
