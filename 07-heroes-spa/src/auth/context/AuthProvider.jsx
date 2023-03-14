876bcf37699209619a81fcdfacb4f07b46422c70
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-14 07:55:39 +0100

    Manterner el usuario activo

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -7,20 +7,33 @@ const initialState = {
     logged: false,
 };
 
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user,
+    };
+};
+
 export const AuthProvider = ({ children }) => {
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, initialState, init);
 
     const login = (name = '') => {
-        const action = {
-            type: types.login,
-            payload: {
-                id: 'ABC',
-                name,
-            },
+        const user = {
+            id: 'ABC',
+            name,
         };
 
+        const action = {
+            type: types.login,
+            payload: user,
+        };
+
+        localStorage.setItem('user', JSON.stringify(user));
+
         dispatch(action);
-    } 
+    }
 
     return (
         <AuthContext.Provider value={ {
