cac9699bc56e4a5f57b6fd3b124373c79de752b5
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-15 07:37:31 +0100

    Rutas públicas

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -3,13 +3,21 @@ import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
 import { Navbar } from '../ui';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
 
-                <Route path="login" element={ <LoginPage /> } />
+                <Route path="login" element={
+                    <PublicRoute>
+                        {/* <LoginPage /> */ }
+                        <Routes> {/* Si queremos varias rutas públicas, mejor así */ }
+                            <Route path="/*" element={ <LoginPage /> } />
+                        </Routes>
+                    </PublicRoute>
+                } />
 
                 <Route path="/*" element={
                     <PrivateRoute>
