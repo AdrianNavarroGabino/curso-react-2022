c070f9dd6c051ff8f1a320068279384fa7a97090
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-18 13:24:52 +0100

    Pruebas con los queryParams

--- 07-heroes-spa/tests/heroes/pages/SearchPage.test.jsx
+++ 07-heroes-spa/tests/heroes/pages/SearchPage.test.jsx
@@ -3,7 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { SearchPage } from '../../../src/heroes/pages/SearchPage';
 
 describe('Pruebas en <SearchPage />', () => {
-    test('Debe mostrarse correctamente con valores por defecto', () => { 
+    test('Debe mostrarse correctamente con valores por defecto', () => {
         const { container } = render(
             <MemoryRouter>
                 <SearchPage />
@@ -11,5 +11,19 @@ describe('Pruebas en <SearchPage />', () => {
         );
 
         expect(container).toMatchSnapshot();
-     });
+    });
+
+    test('Debe mostrar a Batman y el input con el valor del queryString', () => {
+        const { container } = render(
+            <MemoryRouter initialEntries={ ['/search?q=batman'] }>
+                <SearchPage />
+            </MemoryRouter>
+        );
+
+        const input = screen.getByRole('textbox');
+        expect(input.value).toBe('batman');
+
+        const img = screen.getByRole('img');
+        expect(img.src).toBe('http://localhost/assets/heroes/dc-batman.jpg');
+    });
 });
