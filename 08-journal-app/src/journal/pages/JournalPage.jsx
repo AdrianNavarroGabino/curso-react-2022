de87f3d469629e24c1033bed972986c96a398949
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-21 07:27:14 +0100

    JournalLayout y JournalPage

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,15 @@
 import { Typography } from '@mui/material';
-import { MailOutline } from '@mui/icons-material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
     return (
-        <>
-        <Typography variant="h1">JournalPage</Typography>
-        <MailOutline />
-        </>
+        <JournalLayout>
+            <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quisquam numquam nesciunt neque necessitatibus vitae reprehenderit deserunt alias enim quibusdam fugiat explicabo, ducimus, molestiae dolor sed aperiam doloremque harum. Quasi!
+            </Typography>
+
+            {/* NothingSelected */}
+            {/* NoteView */}
+        </JournalLayout>
     )
 }
