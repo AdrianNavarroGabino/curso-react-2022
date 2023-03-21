70e22ce210e0cd245d45f1d7496fcbcea13ec4d8
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-21 07:58:06 +0100

    NothingSelectedView - No hay nada seleccionado

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,15 +1,13 @@
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quisquam numquam nesciunt neque necessitatibus vitae reprehenderit deserunt alias enim quibusdam fugiat explicabo, ducimus, molestiae dolor sed aperiam doloremque harum. Quasi!
-            </Typography>
+            <NothingSelectedView />
 
-            {/* NothingSelected */}
-            {/* NoteView */}
+            {/* NoteView */ }
         </JournalLayout>
     )
 }
