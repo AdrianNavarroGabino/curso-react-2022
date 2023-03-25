a23c55966d090b20510da9afe201fff72d5e05ac
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-25 16:11:29 +0100

    Mostrar los pokemons paginadamente

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,24 +1,31 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
 
 export const PokemonApp = () => {
     const dispatch = useDispatch();
+    const { pokemons, isLoading, page } = useSelector((state) => state.pokemons);
 
     useEffect(() => {
         dispatch(getPokemons());
     }, []);
-    
+
     return (
         <>
             <h1>PokemonApp</h1>
             <hr />
+            <span>Loading: { isLoading ? 'True' : 'False' }</span>
 
             <ul>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
+                { pokemons?.map((pokemon) => <li key={pokemon.name}>{pokemon.name}</li>) }
             </ul>
+
+            <button
+                disabled={ isLoading }
+                onClick={ () => dispatch(getPokemons(page)) }
+            >
+                Next
+            </button>
         </>
     )
 }
--- 09-toolkit-redux/src/store/slices/pokemon/thunks.js
+++ 09-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -6,7 +6,6 @@ export const getPokemons = (page = 0) => {
     return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
 
-        // TODO: Realizar petición http
         const { data: { results: pokemons } } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
 
         dispatch(setPokemons({ pokemons, page: page + 1 }));
