4d68493c75d42df1856bd4ef44573ecd7f8ad465
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-03-26 13:27:11 +0200

    Obtener un Todo por ID

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,7 +1,19 @@
-import { useGetTodosQuery } from './store/apis'
+import { useState } from 'react';
+import { useGetTodoQuery, useGetTodosQuery } from './store/apis'
 
 export const TodoApp = () => {
-    const { data: todos = [], isLoading } = useGetTodosQuery();
+    const [todoId, setTodoId] = useState(1);
+    // const { data: todos = [], isLoading } = useGetTodosQuery();
+    const { data: todo = {}, isLoading } = useGetTodoQuery(todoId);
+
+    const nextTodo = () => {
+        setTodoId(todoId + 1);
+    }
+
+    const prevTodo = () => {
+        setTodoId(todoId - 1);
+    }
+
     return (
         <>
             <h1>Todos - RTK Query</h1>
@@ -9,18 +21,21 @@ export const TodoApp = () => {
 
             <h4>isLoading... { isLoading ? 'True' : 'False' }</h4>
 
-            <pre>...</pre>
+            <pre>{ JSON.stringify(todo) }</pre>
 
-            <ul>
+            {/* <ul>
                 { todos.map((todo) => (
                     <li key={ todo.id }>
                         <strong>{ todo.completed ? 'DONE' : 'Pending' } </strong>
                         { todo.title }
                     </li>
                 )) }
-            </ul>
+            </ul> */}
 
-            <button>
+            <button onClick={ prevTodo }>
+                Prev Todo
+            </button>
+            <button onClick={ nextTodo }>
                 Next Todo
             </button>
         </>
