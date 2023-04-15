e2172a2aeebb8e6384360d899638482815f00564
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-04-15 09:51:19 +0200

    Mostrar el mensaje de error de autenticación

--- 08-journal-app/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,9 +1,9 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Alert, Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useMemo, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { startCreatingUserWithEmailPassword } from '../../store/auth';
 
 const formData = {
@@ -21,6 +21,9 @@ const formValidations = {
 export const RegisterPage = () => {
     const dispatch = useDispatch();
     const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const { status, errorMessage } = useSelector((state) => state.auth);
+    const isCheckingAuthentication = useMemo(() => status === 'checking', [status]);
 
     const {
         displayName,
@@ -92,8 +95,13 @@ export const RegisterPage = () => {
                     </Grid>
 
                     <Grid container spacing={ 2 } sx={ { mb: 2, mt: 1 } }>
+                        { errorMessage && (
+                            <Grid item xs={ 12 }>
+                                <Alert severity='error'>{ errorMessage }</Alert>
+                            </Grid>
+                        ) }
                         <Grid item xs={ 12 }>
-                            <Button type="submit" variant="contained" fullWidth>
+                            <Button disabled={ isCheckingAuthentication } type="submit" variant="contained" fullWidth>
                                 Crear cuenta
                             </Button>
                         </Grid>
