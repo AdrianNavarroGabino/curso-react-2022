916e89413145ae1044b169c40e95c4e40bc48ffb
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-05-03 08:13:41 +0200

    Cargar notas de Firestore

--- 08-journal-app/src/hooks/useCheckAuth.js
+++ 08-journal-app/src/hooks/useCheckAuth.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FirebaseAuth } from '../firebase/config';
 import { login, logout } from '../store/auth';
+import { startLoadingNotes } from '../store/journal';
 
 export const useCheckAuth = () => {
     const { status } = useSelector(state => state.auth);
@@ -16,7 +17,8 @@ export const useCheckAuth = () => {
 
             const { uid, email, displayName, photoURL } = user;
 
-            return dispatch(login({ uid, email, displayName, photoURL }));
+            dispatch(login({ uid, email, displayName, photoURL }));
+            dispatch(startLoadingNotes());
         })
     }, []);
 
--- 08-journal-app/src/store/journal/thunks.js
+++ 08-journal-app/src/store/journal/thunks.js
@@ -1,11 +1,12 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
-import { addNewEmptyNote, setActiveNote, savingNewNote } from '.';
+import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes } from '.';
 import { FirebaseDB } from '../../firebase/config';
+import { loadNotes } from '../../helpers';
 
 export const startNewNote = () => {
     return async (dispatch, getState) => {
         dispatch(savingNewNote);
-        
+
         const { uid } = getState().auth;
 
         const newNote = {
@@ -17,9 +18,22 @@ export const startNewNote = () => {
         const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
         await setDoc(newDoc, newNote);
 
-        newNote.id= newDoc.id;
+        newNote.id = newDoc.id;
 
         dispatch(addNewEmptyNote(newNote));
         dispatch(setActiveNote(newNote));
+    }
+}
+
+export const startLoadingNotes = () => {
+    return async (dispatch, getState) => {
+        const { uid } = getState().auth;
+
+        if(!uid) {
+            throw new Error('El uid del usuario no existe')
+        }
+
+        const notes = await loadNotes(uid);
+        dispatch(setNotes(notes));
     }
 }
